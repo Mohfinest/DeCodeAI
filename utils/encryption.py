f8e20ba58dab77b0e1c76ca3a434b28f3cfa2b4c
Musa Abdulmumin <abdulmuminm97@gmail.com>
2025-07-24 13:31:11 +0100

    Update encryption.py

--- utils/encryption.py
+++ utils/encryption.py
@@ -1,22 +1,9 @@
-from Cryptodome.Cipher import AES
 import base64
-import hashlib
 
-def pad(text):
-    pad_len = AES.block_size - len(text) % AES.block_size
-    return text + chr(pad_len) * pad_len
+def encrypt_text(text: str) -> str:
+    encoded = base64.b64encode(text.encode('utf-8')).decode('utf-8')
+    return encoded
 
-def unpad(text):
-    return text[:-ord(text[-1])]
-
-def encrypt_text(plain_text, secret_key):
-    key = hashlib.sha256(secret_key.encode()).digest()
-    cipher = AES.new(key, AES.MODE_ECB)
-    encrypted = cipher.encrypt(pad(plain_text).encode())
-    return base64.b64encode(encrypted).decode()
-
-def decrypt_text(cipher_text, secret_key):
-    key = hashlib.sha256(secret_key.encode()).digest()
-    cipher = AES.new(key, AES.MODE_ECB)
-    decrypted = cipher.decrypt(base64.b64decode(cipher_text))
-    return unpad(decrypted.decode())
+def decrypt_text(encoded_text: str) -> str:
+    decoded = base64.b64decode(encoded_text.encode('utf-8')).decode('utf-8')
+    return decoded
